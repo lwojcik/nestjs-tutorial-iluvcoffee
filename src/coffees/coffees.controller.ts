eee373c1557678ca2fe4078258fdb94e151a801f
Łukasz Wójcik <lwojcik@lukem.net>
2022-05-02 22:55:04 +0200

    Auto-transform payloads to DTO instances

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,8 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Post,
@@ -25,12 +23,13 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id') id: number) {
+    return this.coffeesService.findOne('' + id);
   }
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeesService.create(createCoffeeDto);
   }
 
