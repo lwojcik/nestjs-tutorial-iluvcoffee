bad9a842eb1d00512b64218e3fc9e1ee508c89ed
Łukasz Wójcik <lwojcik@lukem.net>
2022-06-14 22:20:11 +0200

    Value based Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,14 +6,17 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+// mock implementation
+class MockCoffeesService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
+  // Custom mock provider:
   // providers: [
   //   {
   //     provide: CoffeesService,
-  //     useClass: CoffeesService,
+  //     useClass: MockCoffeesService(),
   //   },
   // ],
   exports: [CoffeesService],
