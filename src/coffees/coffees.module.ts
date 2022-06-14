1a3757cdd75f99f5e8fc06ec2ffb2b430ba49919
Łukasz Wójcik <lwojcik@lukem.net>
2022-06-14 22:28:40 +0200

    Non-class-based Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
 // mock implementation
-class MockCoffeesService {}
+// class MockCoffeesService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+  ],
+  exports: [CoffeesService],
   // Custom mock provider:
   // providers: [
   //   {
@@ -19,6 +24,5 @@ class MockCoffeesService {}
   //     useClass: MockCoffeesService(),
   //   },
   // ],
-  exports: [CoffeesService],
 })
 export class CoffeesModule {}
